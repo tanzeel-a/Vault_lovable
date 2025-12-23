ec31540f7c2de541fff73865eda3a280cc1ea098
tanzeel-a <ahmadtanzeel303@gmail.com>
2025-12-24 02:58:39 +0530

    Update .gitignore and remove .next/ and .env from tracking

--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_globals_91e4631d.css",
-  "static/chunks/node_modules_f6e9f540._.js",
-  "static/chunks/src_4bd93885._.js"
-],
-    source: "dynamic"
-});
